566476d5615946c16f7a547997b62480ad257542
Egbedokun Olayinka <egbedokunolayinka@gmail.com>
2020-12-04 02:20:04 +0100

    added some functionality to hot-and-cold project

--- hot-and-cold/main.js
+++ hot-and-cold/main.js
@@ -1,5 +1,7 @@
 let randomNumber;
-let formerNumber;
+let formerNumber = null;
+let randomUp = [];
+let randomDown = [];
 
 // dom elements
 const body = document.querySelector("body");
@@ -8,6 +10,7 @@ const choice = document.querySelector("#choice");
 const container = document.querySelector(".container");
 const temp = document.querySelector("#temp-id");
 const newGame = document.querySelector("#newGame");
+const submitBtn = document.querySelector("#submitBtn");
 
 // event listeners
 document.addEventListener("DOMContentLoaded", chooseRandomNumber);
@@ -16,7 +19,14 @@ newGame.addEventListener("click", resetGame);
 
 // functions
 function chooseRandomNumber() {
+  randomUp = [];
+  randomDown = [];
   randomNumber = Math.floor(Math.random() * 101);
+
+  for (i = 1; i <= 20; i++) {
+    randomUp.push(randomNumber + i);
+    randomDown.push(randomNumber - i);
+  }
 }
 
 function submitForm(e) {
@@ -24,10 +34,32 @@ function submitForm(e) {
 
   temp.classList.add("temp");
 
-  if (choice.value < randomNumber) {
-    temp.innerText = "COLD";
+  if (choice.value == randomNumber) {
+    temp.innerText = "CORRECT!!!";
+    submitBtn.setAttribute("disabled", true);
+    return;
+  }
+
+  if (formerNumber) {
+    if (
+      randomUp.includes(+choice.value) ||
+      randomDown.includes(+choice.value)
+    ) {
+      temp.innerText = "GETTING HOTTER!!!";
+      formerNumber = +choice.value;
+    } else {
+      temp.innerText = "GETTING COLDER!!!";
+      formerNumber = +choice.value;
+    }
+    return;
+  }
+
+  if (randomUp.includes(+choice.value) || randomDown.includes(+choice.value)) {
+    temp.innerText = "HOT!!!";
+    formerNumber = +choice.value;
   } else {
-    temp.innerText = "HOT";
+    temp.innerText = "COLD!!!";
+    formerNumber = +choice.value;
   }
 }
 
@@ -36,4 +68,5 @@ function resetGame() {
   choice.value = "";
   temp.classList.remove("temp");
   temp.innerText = "";
+  submitBtn.removeAttribute("disabled");
 }
